a40b4612b31ade174c9b1a895458d5cca992ba9f
MALLUVALASAJAIDEEP <malluvalasajaideep@gmail.com>
2025-06-27 15:55:17 +0530

    Test Commit Passed

--- python-test-file.py
+++ python-test-file.py
@@ -1,12 +1,6 @@
 import requests
 
 url = "http://localhost:8000/heroes"
-data ={
-    "id": 1,
-    "name": "my hero",
-    "secret_name": "austing",
-    "age": 12
-
-}
-response =requests.post(url, json=data)
+data = {"id": 1, "name": "my hero", "secret_name": "austing", "age": 12}
+response = requests.post(url, json=data)
 print(response.json())
